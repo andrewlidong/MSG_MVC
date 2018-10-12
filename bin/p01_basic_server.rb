820218972179b7ce6a68d46d40c08f1793e6f376
Andrew Dong <andrewdong1994@gmail.com>
2018-10-12 14:07:21 -0400

    updated files

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -1,14 +1,14 @@
 require 'rack'
 
 app = Proc.new do |env|
-    req = Rack::Request.new(env)
-    res = Rack::Response.new
-    res['Content-Type'] = "text/type"
-    res.write(req.path)
-    res.finish
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = "text/text"
+  res.write(req.path)
+  res.finish
 end
 
 Rack::Server.start(
-    app: app,
-    Port: 3000
+  app: app,
+  Port: 3000
 )
